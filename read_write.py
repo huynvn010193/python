7dbd841a1db03cac91d7ea1e77d69b56e601019e
huynvn <Huy.Nguyen@navigosgroup.com>
2023-02-01 16:32:42 +0700

    8.String format

--- read_write.py
+++ read_write.py
@@ -1,17 +1,19 @@
 CURRENT_YEAR = 2023
+METER_TO_FEET = 3.281
 
-print('Your frist name')
-firstname = input()
+firstname = input('Your frist name: ')
+lastname = input('Your last name: ')
+year_born = input('When you was born: ')
+height_meter = input('Your height (meter): ')
 
-print('Your last name')
-lastname = input()
-
-print('Your name is: ' + firstname + ' ' + lastname)
-
-print('When you was born')
-year_born = input()
 year_born = int(year_born) #overide
-
 age = CURRENT_YEAR - year_born
 
-print("You are " + str(age) + " years old in " + str(CURRENT_YEAR))
+height_meter = float(height_meter)
+height_feet = height_meter * METER_TO_FEET
+height_feet = round(height_feet, 1)
+
+print('\n-----')
+print('Your name is: ' + firstname + ' ' + lastname)
+print("{2} is {0} years old in {1}".format(age,CURRENT_YEAR, firstname))
+print("You are " + str(height_feet) + " feet tall")
