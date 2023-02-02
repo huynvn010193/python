dece7976c0ee988fb37e8fe67a58043848c3b69c
huynvn <Huy.Nguyen@navigosgroup.com>
2023-02-02 11:15:22 +0700

    10.Boolean

---
+++ if_else.py
@@ -0,0 +1,15 @@
+age = 16
+is_con_nit = False #initialise the variable
+if age < 10:
+  is_con_nit = True
+
+if is_con_nit:
+  print("con nit")
+elif age == 18:
+  print("18 tuoi tre trau")
+elif age < 18:
+  print("tre trau")
+  if age >= 15 and age <= 17:
+    print("sieu tre trau")
+else:
+  print("nguoi lon")
