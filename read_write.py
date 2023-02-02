aada05311665f85f03e64f6bde26ba3ff6b5bfec
huynvn <Huy.Nguyen@navigosgroup.com>
2023-02-02 17:18:26 +0700

    13. AND

--- read_write.py
+++ read_write.py
@@ -17,3 +17,30 @@ print('\n-----')
 print('Your name is: ' + firstname + ' ' + lastname)
 print("{2} is {0} years old in {1}".format(age,CURRENT_YEAR, firstname))
 print("You are " + str(height_feet) + " feet tall")
+
+gender_input = input("Are you male (yes/no): ")
+is_name = None
+
+if (gender_input == "yes") or (gender_input == "y") or (gender_input == "Yes"):
+  is_male = True
+elif gender_input == "no" or (gender_input == "n") or (gender_input == "No"):
+  is_male = False
+else:
+  is_male = None
+
+if is_male is None:
+  print("Invalid Answer")
+elif is_male == True:
+  if height_feet > 6.5:
+    print("You are very tall as a man")
+  elif height_feet > 6.0:
+    print("You are tall as a man")
+  else:
+    print("You are short as a man")
+elif is_male == False:
+  if height_feet > 5.7:
+    print("You are tall as a girl")
+  else:
+    print("You are short as a girl")
+else:
+  print("system error, variable 'is_male' is not conrrect")
